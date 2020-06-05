92d49e4e58e6337e13cd37cd0b302c0f4d4fc887
mpaolodr <mpaolodr_1504@yahoo.com>
2020-06-04 20:34:45 -0400

    ex2 solution added

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,32 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    flight = {}
+    route = []
+
     # Your code here
+
+    # create key for each ticket
+    for i in range(length):
+        key = tickets[i].source
+        val = tickets[i].destination
+
+        # make source as key
+        flight[key] = val
+
+    # if key is NONE, it's the beginning of route
+    key = flight["NONE"]
+
+    # if key has a value of NONE, that the last destination
+    # until then, keep adding to route list
+
+    while key != "NONE":
+        route.append(key)
+
+        # determine the next destination
+        key = flight[key]
+
+    # key has a value of NONE so while loop won't execute
+    route.append(key)
 
     return route
